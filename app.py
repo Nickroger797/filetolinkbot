dc9ade58db4497f8b57278ed6846f970f16a144d
Nickroger797 <115403112+Nickroger797@users.noreply.github.com>
2023-01-07 16:39:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 def hello_world():
    return 'GreyMatters_Bot'
 
-#Ex https://Greymattersbot:ghp_147bkkabcdefgh@github.com/Greymattersbot/Mogenius
-os.system("git clone https://github.com/Nickroger797/filetolinkbot okk && cd okk && pip3 install -U -r requirements.txt && nohup python -m Adarsh &")
+if __name__ == "__main__":
+    app.run()
