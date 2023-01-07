7dc79f618b70a8a573bfe2d859c028f9179c7573
Nickroger797 <115403112+Nickroger797@users.noreply.github.com>
2023-01-07 16:27:49 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+   return 'GreyMatters_Bot'
+
+#Ex https://Greymattersbot:ghp_147bkkabcdefgh@github.com/Greymattersbot/Mogenius
+os.system("git clone https://github.com/Nickroger797/filetolinkbot okk && cd okk && pip3 install -U -r requirements.txt && nohup python -m Adarsh &")
