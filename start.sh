687f30f8e33a6049a50674980e8edf78e20941e4
Nickroger797 <115403112+Nickroger797@users.noreply.github.com>
2023-01-07 16:42:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@ echo "Cloning Repo...."
 git clone https://github.com/Nickroger797/filetolinkbot.git /filetolinkbot
 cd /filetolinkbot
 pip3 install -U -r requirements.txt
-echo "⚡️⚡️ Starting advancesmdiskbot..."
-python3 main.py
+echo "⚡️⚡️ Starting filetolinkbot..."
+python -m Adarsh
